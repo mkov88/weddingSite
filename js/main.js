849e68bfe97a4f3b6efc0ea1f83d644fb95b5180
Michael Kalmykov <Michael.kalmykov@liveokbank.com>
2016-12-27 15:18:15 -0500

    fumbled with sidebar no dice

--- js/main.js
+++ js/main.js
@@ -59,3 +59,27 @@ function smoothScroll(eID) {
 // ==============================================
 // End excessive code for smooth scroll
 // ==============================================
+
+
+
+// ==============================================
+// Start sidebar Stickyness
+// ==============================================
+$(function() {
+    var offset = $("#sidebar").offset();
+    var topPadding = 15;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > offset.top) {
+            $("#sidebar").stop().animate({
+                marginTop: $(window).scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $("#sidebar").stop().animate({
+                marginTop: 0
+            });
+        };
+    });
+});
+// ==============================================
+// END sidebar Stickyness
+// ==============================================
